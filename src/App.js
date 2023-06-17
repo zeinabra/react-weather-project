58a73c93b30dae6d5e15e34fd15d0b61e131d74d
zeinabra <127112905+zeinabra@users.noreply.github.com>
2023-06-17 17:30:05 +0430

    add css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Weather from "./Weather";
 import './App.css';
-import "./Weather.css";
+
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       
       <Weather />
       <footer>
         coded by Zeinab Rasouli <a href="https://github.com/zeinabra/react-weather-project" target="_blank">open-sourced</a>
       </footer>
+     </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -3,26 +3,28 @@ import "./Weather.css";
 export default function Weather(){
     return(
         <div className="Weather">
-            <form>
+            <form className="mt-3">
                 <div className="row">
                     <div className="col-9">
-                        <input type="search" placeholder="Enter a city ..." className="form-control" />
+                        <input type="search" placeholder="Enter a city ..." className="form-control" autoFocus="on" />
                     </div>
                     <div className="col-3">
-                        <input type="submit" value="search" className="btn btn-primary"/>
+                        <input type="submit" value="search" className="btn btn-primary w-100"/>
                     </div>
                 </div>
 
             </form>
-            <h1>New York</h1>
+            <h1 className="mt-3 mb-1">New York</h1>
             <ul>
                 <li>Wednesday 07:00</li>
                 <li>cloudy</li>
             </ul>
-            <div className="row">
+            <div className="row mt-3">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
-                    17 °C
+                    <div className="d-flex">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" className="image" />
+                    <span className="tempreture">17</span> <span className="unit">°C</span>
+                    </div>
                 </div>
                 <div className="col-6">
                     <ul>
