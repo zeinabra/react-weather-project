bd2dbd949afdde816b6dd449b05b299904d3e106
zeinabra <127112905+zeinabra@users.noreply.github.com>
2023-06-17 12:28:22 +0430

    add github url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
+      <footer>
+        coded by Zeinab Rasouli <a href="https://github.com/zeinabra/react-weather-project" target="_blank">open-sourced</a>
+      </footer>
     </div>
   );
 }
