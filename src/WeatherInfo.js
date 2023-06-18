4de656a26d513df7f31b6731d35a5e280666ec8a
zeinabra <127112905+zeinabra@users.noreply.github.com>
2023-06-18 23:18:52 +0430

    add unit convershion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,7 @@
 import React from "react";
 import FreindlyDate from "./FreindlyDate";
+import WeatherTemperature from "./WeatherTemperature";
+import "./Weather.css";
 
 export default function WeatherInfo(props){
     return(
@@ -7,20 +9,20 @@ export default function WeatherInfo(props){
           <h1 className="mt-3 mb-1">{props.data.cityName}</h1>
             <ul>
                 <li><FreindlyDate date={props.data.date} /></li>
-                <li className="text-capitalize">{props.data.description}</li>
+                <li className="text-capitalize mt-1">{props.data.description}</li>
             </ul>
             <div className="row mt-3">
                 <div className="col-6">
                     <div className="d-flex">
                     <img src={props.data.icon} alt={props.data.description} className="image" />
-                    <span className="tempreture">{props.data.temperature}</span> <span className="unit">°C</span>
+                    <WeatherTemperature celcius={props.data.temperature} />
                     </div>
                 </div>
                 <div className="col-6">
                     <ul>
                         
                         <li>Humidity: {props.data.humidity}%</li>
-                        <li>Wind: {props.data.wind} km/h</li>
+                        <li className="mt-1">Wind: {props.data.wind} km/h</li>
                     </ul>
                 </div>
             </div>
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,6 +1,8 @@
 import React,{useState} from "react";
+import "./Weather.css";
 
 export default function WeatherTemperature(props){
+    
 let[unit,setUnit]=useState("celsius");
 
 function convertToFahrenhiet(event){
@@ -13,16 +15,17 @@ function convertToCelcius(){
 if(unit==="celsius"){
     return(
         <div className="WeatherTemperature">
-        <span className="tempereture">{props.celcius}</span>
+        <span className="temperature">{props.celcius}</span>
          <span className="unit">
          °C |<a href="/" onClick={convertToFahrenhiet}>°F</a>
          </span>
         </div>
     );
 }else {
-     let fahrenhiet =(props.celcius*9)/5+32;
-        return(<div className="WeatherTemperature">
-        <span className="tempereture">{fahrenhiet}</span>
+     let fahrenhiet =Math.round((props.celcius*9)/5+32);
+        return(
+        <div className="WeatherTemperature">
+        <span className="temperature">{fahrenhiet}</span>
          <span className="unit">
          <a href="/" onClick={convertToCelcius}>°C </a>|°F
          </span>
